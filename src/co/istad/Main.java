8cf2350cc357cee602959641e576db671b113c55
rtn <eav.rotana@gmail.com>
2025-12-01 19:50:19 +0700

    remvoe test code

--- src/co/istad/Main.java
+++ src/co/istad/Main.java
@@ -1,8 +1,4 @@
 package co.istad;
-
-import co.istad.view.MenuUI;
-import co.istad.service.CartService;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -10,28 +6,5 @@ public class Main {
         // MenuUI menu = new MenuUI();
         // menu.start();
 
-        testCartServiceIntegration();
-    }
-
-    public static void testCartServiceIntegration() {
-        MenuUI menu = new MenuUI();
-
-        CartService cartService = menu.getCartService();
-
-        System.out.println("--- Starting CartService Test ---");
-
-        System.out.println("\n[Test 1] Adding 3 of Product ID 1...");
-        cartService.addItemToCart(1, 3);
-
-        System.out.println("\n[Test 2] Adding 5 of Product ID 2...");
-        cartService.addItemToCart(2, 5);
-
-        System.out.println("\n[Test 3] Adding Product ID 99 (Non-existent)...");
-        cartService.addItemToCart(99, 1);
-
-        System.out.println("\n[Test 4] Executing Checkout...");
-        cartService.checkout();
-
-        System.out.println("\n--- Test Complete. Check products.csv for updated stock levels! ---");
     }
 }
