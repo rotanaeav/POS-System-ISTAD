b5b2202088ca2fa03fcfb5175ad17a526fee2777
rtn <eav.rotana@gmail.com>
2025-12-01 19:44:32 +0700

    removed test code in MenuUI

--- src/co/istad/view/MenuUI.java
+++ src/co/istad/view/MenuUI.java
@@ -1,16 +1,7 @@
 package co.istad.view;
-import co.istad.entity.User;
-import co.istad.dao.impl.ProductFileDao;
-import co.istad.service.AuthService;
-import co.istad.service.CartService;
-import co.istad.service.ProductService;
-import co.istad.dao.ProductDao;
+
 
 public class MenuUI {
-    private final ProductFileDao productFileDao = new ProductFileDao();
-    private final ProductService productService = new ProductService();
-    private final AuthService authService = new AuthService();
-    private final CartService cartService = new CartService(productFileDao);
     public void start() {
         // if (user == null) {
         //                printinfo(Exiting program.....);
@@ -23,10 +14,6 @@ public class MenuUI {
 
     }
     private void sellerMenu() {
-    }
-
-    public CartService getCartService() {
-        return this.cartService;
     }
     
 }
